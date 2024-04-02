06c6416088f340a6dc9615c4ad5dad103f6bc626
unknown <muhammaddanishgtr24@gmail.com>
2024-04-03 00:53:53 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answers=await inquirer.prompt([
+    {message:"Enter Your First Number:",type:"number",name:"Firstnumber"},
+    {message:"Enter Your Second Number:",type:"number",name:"Secondnumber"},
+    {message:"Select your Operator:",type:"list",name:"Operator",choices:["addition","subtraction","multiplication","division"]}
+
+])
+
+if (answers.Operator==="addition"){console.log(answers.Firstnumber+answers.Secondnumber)}
+else if (answers.Operator==="subtraction"){console.log(answers.Firstnumber-answers.Secondnumber)}
+else if (answers.Operator==="multiplication"){console.log(answers.Firstnumber*answers.Secondnumber)}
+else if (answers.Operator==="division"){console.log(answers.Firstnumber/answers.Secondnumber)}
+else{console.log("Please Select a Valid Operator")}
